c62bdf6da802edc58c61accb8a183bbe54364869
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2018-08-08 16:20:09 -0300

    Working on the saddle file.

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,16 @@
+
 def saddle_points(matrix):
-    pass
+
+    transpose = list(zip(*matrix))
+    for row, column in zip(matrix, transpose):
+        i = row.index(max(row))
+        column = transpose[i]
+        j = column.index(min(column))
+        if max(row) == min(column):
+            print(j, i)
+
+
+# matrix = [[9, 8, 7], [5, 3, 2], [6, 6, 7]]
+matrix = [[4, 5, 4], [3, 5, 5], [1, 5, 4]]
+# matrix = [[6, 7, 8], [5, 5, 5], [7, 5, 6]]
+saddle_points(matrix)
