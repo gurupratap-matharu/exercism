4836f349f1859e05c005669af8338e74b4ae0026
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2018-08-09 12:19:43 -0300

    Got the saddle points exercise to be working.

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,16 +1,33 @@
 
 def saddle_points(matrix):
+    """
+    Returns a set of indices of saddle points in a given matrix.
+    Saddle points are elements
+        a. Highest in the row
+        b. Lowest in the corresponding column
+    """
 
-    transpose = list(zip(*matrix))
-    for row, column in zip(matrix, transpose):
-        i = row.index(max(row))
-        column = transpose[i]
-        j = column.index(min(column))
-        if max(row) == min(column):
-            print(j, i)
+    # We check if all rows of the given matrix are of uniform length
 
+    if not all([len(row) == len(matrix[0]) for row in matrix]):
+        raise ValueError('Matrix is not uniform in structure.')
 
-# matrix = [[9, 8, 7], [5, 3, 2], [6, 6, 7]]
-matrix = [[4, 5, 4], [3, 5, 5], [1, 5, 4]]
-# matrix = [[6, 7, 8], [5, 5, 5], [7, 5, 6]]
-saddle_points(matrix)
+    saddle = []  # We'll store the results here and return it.
+
+    transpose = list(zip(*matrix))  # This is a transpose of the original. Rows and columns are basically inverted.
+
+    for row_index, row in enumerate(matrix):
+
+        highest_element = max(row)
+
+        for column_index, element in enumerate(row):  # Iterate through each sub matrix
+
+            if element == highest_element:  # There can be multiple largest elements!
+
+                lowest_element = min(transpose[column_index])
+
+                if highest_element == lowest_element:
+
+                    saddle.append((row_index, column_index))
+
+    return set(saddle)
