144a4662c3898b89cecb767931ebe90bc9588e5e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2018-08-25 14:38:51 -0300

    Book Store problem has passed all unittests.

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,4 +1,8 @@
 def calculate_total(books):
+    """
+    Returns the minimum cost of books for any conceivable shopping cart
+    """
+    # We store the discounted price of occurences of each type
     d = {
         0: 0,
         1: 800,
@@ -9,20 +13,35 @@ def calculate_total(books):
     }
 
     total_cost = 0
+    total_cost_lst = 0
 
-    counts = [books.count(i) for i in range(1, 6)]
+    counts = [books.count(i) for i in range(1, 6)]  # Tells us the number or each type of book in the shopping cart.
+    counts.sort(reverse=True)
 
+    lst = counts.copy()
+
+    # First we find the total cost descending from highest groupings of 5 to the lowest groupings of 1.
+    # We do this since higher groupings have larger absolute discounts.
     while counts:
 
-        print(counts)
-        counts = [x for x in counts if x != 0]
+        counts = [x for x in counts if x != 0]  # Removes zeroes
+        uniques = len(counts)  # This the current highest unique combination
+        counts = [x - 1 if x > 0 else 0 for x in counts]  # Reduce 1 item from entire basket.
 
-        uniques = len(counts)
-        total_cost += d[uniques]
-        counts = [x - 1 if x > 0 else 0 for x in counts]
+        total_cost += d[uniques]  # Store the total cost here
 
-    return total_cost
+    # Now we calculate costs using preferred groupings of 4! In some cases this yields better discounts.
+    while lst:
 
+        lst = [x for x in lst if x != 0]  # Remove zeroes
 
-if __name__ == '__main__':
-    print(calculate_total([1, 1, 2, 2, 3, 3, 4, 5, 1, 1, 2, 2, 3, 3, 4, 5]))
+        if len(lst) >= 4:
+            total_cost_lst += d[4]
+            for i in range(4):
+                lst[i] -= 1  # Reduce 1 item from first four elements.
+
+        else:
+            total_cost_lst += d[len(lst)]
+            lst = [x - 1 if x > 0 else 0 for x in lst]  # Reduce 1 item from entire basket.
+
+    return min(total_cost_lst, total_cost)
