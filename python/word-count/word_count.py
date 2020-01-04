1b389e0dbf7507681e07b91306522a0cc1c31b47
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-01-04 16:32:10 -0300

    Using REGEX to identify word count and its passing all edge test cases :)

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,13 @@
-def word_count(phrase):
-    lst = list(phrase)
+def count_words(phrase):
+    """
+    Returns a dict with count of each word in a phrase
+    keys are the words and values the count of occurrence.
+    """
+    import re
+    import string
+    from collections import Counter
+    phrase = phrase.lower()
+    tokens = re.findall(r'[0-9a-zA-Z\']+', phrase)
+    tokens = [word.strip(string.punctuation) for word in tokens]
+    counts = Counter(tokens)
+    return counts
