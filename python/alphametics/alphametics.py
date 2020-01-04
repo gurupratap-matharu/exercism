1938447d4b5c55c2e9d0f2b344de0b7c116a37b5
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-01-04 20:38:13 -0300

    wip for alphametics

--- python/alphametics/alphametics.py
+++ python/alphametics/alphametics.py
@@ -1,6 +1,13 @@
-def solve(puzzle): 
-    print(list(puzzle))
+def solve(puzzle):
+    """
+    Returns the integer value of each character of the alphametics
+    puzzle
+    """
+    puzzle = puzzle.split(' == ')
+    return puzzle
 
 
-
-solve("SEND + MORE == MONEY")
+if __name__ == '__main__':
+    PUZZLE = "AND + A + STRONG + OFFENSE + AS + A + GOOD == DEFENSE"
+    RESULT = solve(puzzle=PUZZLE)
+    print(RESULT)
