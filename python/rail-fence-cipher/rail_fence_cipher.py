f01dd8609e6320172c9535a6f6c5b8135697ad59
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-01-05 11:12:08 -0300

    WiP for rail fence

--- python/rail-fence-cipher/rail_fence_cipher.py
+++ python/rail-fence-cipher/rail_fence_cipher.py
@@ -1,6 +1,30 @@
 def encode(message, rails):
-    pass
+    """
+    Encodes a message as per the rails fence algorithm.
+    Params:
+    rails: int: defines the layers we use to encode the message.
+    """
+    encoded_message = []
+    lane = 1
+    for i in range(len(message)):
+        if lane == 1:
+            count = 1
+        if lane == rails:
+            count = -1
+        encoded_message.append(lane)
+        lane += count
+    return encoded_message
 
 
 def decode(encoded_message, rails):
-    pass
+    """
+    Decodes a message as per the rails fence algorithm.
+    Params:
+    rails: int: defines the layers we used to encode the message.
+    """
+    return
+
+
+if __name__ == '__main__':
+    RESULT = encode(message="EXERCISESFORTHEBRAINTOBESHARP", rails=9)
+    print(RESULT)
