338527872fd33a806b7ac831aec8b0d2cc521f49
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-01-06 10:43:34 -0300

    implemented using list and custom methods. All unit tests are passing. :)

--- python/circular-buffer/circular_buffer.py
+++ python/circular-buffer/circular_buffer.py
@@ -1,67 +1,50 @@
 class BufferFullException(Exception):
-    def __init__(self, message):
-        self.message = message
-        raise Exception(self.message)
+    pass
 
 
 class BufferEmptyException(Exception):
-    def __init__(self, message):
-        self.message = message
-        raise Exception(message)
+    pass
 
 
 class CircularBuffer:
     def __init__(self, capacity):
         """
-        The constructor class which instantiates a buffer and defines its capacity.
+        The constructor class which instantiates an empty buffer with the desired capacity.
         """
         self.capacity = capacity
-        from collections import deque
-        self.buffer = deque([])
+        self.buffer = []
 
     def read(self):
         """
         Returns the older chunk of data from the buffer.
         """
         if len(self.buffer) == 0:
-            BufferEmptyException("Buffer is empty!")
-        return self.buffer.popleft()
+            raise BufferEmptyException("Buffer is empty!")
+        return self.buffer.pop(0)
 
     def write(self, data):
         """
-        If buffer is not full write data to it else raises an exception.
+        If buffer is not full writes data to it else raises an exception.
         """
         if len(self.buffer) >= self.capacity:
-            BufferFullException("Buffer is full!")
+            raise BufferFullException("Buffer is full!")
         for chunk in data:
             self.buffer.append(chunk)
         return self.buffer
 
     def overwrite(self, data):
         """
-        If buffer is not full writes more data else remove the oldest chunk 
-        and then writes new data.
+        If buffer is not full writes more data else remove the oldest chunk and then writes new data.
         """
-        for _, chunk in enumerate(data):
+        for chunk in data:
             # first check if buffer is not full
             if len(self.buffer) >= self.capacity:
-                self.buffer.popleft()
+                self.buffer.pop(0)
             self.buffer.append(chunk)
+
         return self.buffer
 
     def clear(self):
         """Clears the buffer itself"""
         self.buffer.clear()
-
-
-if __name__ == '__main__':
-    c = CircularBuffer(3)
-    c.write('5')
-    c.write('4')
-    c.write('3')
-    c.write('2')
-    c.write('1')
-    print(c.read())
-    print(c.read())
-    print(c.read())
-    print(c.read())
+        return self.buffer
