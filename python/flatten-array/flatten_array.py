e3316bacd785c678f3062811586aff38df244611
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-09-10 12:33:02 +0530

    Remove possible error while flattening iterable of arbitrary length

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,13 +1,20 @@
-def flatten(iterable):
+def iter_flatten(iterable):
+    """
+    Flattens a list or tuple of arbitrary nested depth into one single list.
+    """
 
-    lst = []
-    for row in iterable:
-        if isinstance(row, list):
-            for element in flatten(row):
-                lst.append(element)
+    my_iter = iter(iterable)
+
+    for element in my_iter:
+        if isinstance(element, (list, tuple)):
+            for nested_element in iter_flatten(element):
+                yield nested_element
         else:
-            lst.append(row)
-    return lst
+            yield element
 
 
-print(flatten([0, 1, 2]))
+def flatten(iterable):
+    """
+    Helper method which returns an in-memory list from a generator based flattener."""
+
+    return [x for x in list(iter_flatten(iterable)) if x is not None]
