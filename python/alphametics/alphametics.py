34aaff7c9d4dc7e151bff381f67fd73ecab55f3c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-09-19 19:36:51 +0530

    Implement interface to decode letters to numbers

--- python/alphametics/alphametics.py
+++ python/alphametics/alphametics.py
@@ -1,13 +1,2 @@
 def solve(puzzle):
-    """
-    Returns the integer value of each character of the alphametics
-    puzzle
-    """
-    puzzle = puzzle.split(' == ')
-    return puzzle
-
-
-if __name__ == '__main__':
-    PUZZLE = "AND + A + STRONG + OFFENSE + AS + A + GOOD == DEFENSE"
-    RESULT = solve(puzzle=PUZZLE)
-    print(RESULT)
+    pass
