c8391224596cf185dc721f2e5530aeefc28b6c13
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-09-26 21:56:09 +0530

    Add inorder traversal using iterative method

--- python/binarytree-traversal/inorder.py
+++ python/binarytree-traversal/inorder.py
@@ -1,3 +1,5 @@
+"""A script to traverse a binary tree in order either recursively or iteratively"""
+
 from typing import List, Optional
 
 
@@ -11,12 +13,18 @@ class TreeNode:
 
 
 class Tree:
+    """Represents a Binary Tree"""
+
     def inorder_traversal(self, root: Optional[TreeNode]) -> List[int]:
+        """Traverses a binary tree in order using recursion"""
+
         res = []
         self.helper(node=root, res=res)
         return res
 
     def helper(self, node, res):
+        """Helper recursive method to visit nodes"""
+
         if not node:
             return
 
@@ -24,9 +32,44 @@ class Tree:
         res.append(node.val)
         self.helper(node.right, res)
 
+    def inorder_iterative_traversal(self, root: Optional[TreeNode]) -> List[int]:
+        """Traverses a binary tree iteratively"""
 
-if __name__ == '__main__':
+        node = root
+        res = []
+        stack = []
+
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                node = node.right
+
+        return res
+
+
+if __name__ == "__main__":
     tree = Tree()
-    assert tree.inorder_traversal(root=TreeNode(val=1, left=TreeNode(val=2), right=None)), [2, 1]
-    assert tree.inorder_traversal(root=TreeNode(val=1, left=None, right=TreeNode(
-        val=2, left=TreeNode(val=3), right=None))), [1, 3, 2]
+
+    # recursive method
+    assert tree.inorder_traversal(
+        root=TreeNode(val=1, left=TreeNode(val=2), right=None)
+    ), [2, 1]
+    assert tree.inorder_traversal(
+        root=TreeNode(
+            val=1, left=None, right=TreeNode(val=2, left=TreeNode(val=3), right=None)
+        )
+    ), [1, 3, 2]
+
+    # iterative method
+    assert tree.inorder_iterative_traversal(
+        root=TreeNode(val=1, left=TreeNode(val=2), right=None)
+    ), [2, 1]
+    assert tree.inorder_iterative_traversal(
+        root=TreeNode(
+            val=1, left=None, right=TreeNode(val=2, left=TreeNode(val=3), right=None)
+        )
+    ), [1, 3, 2]
