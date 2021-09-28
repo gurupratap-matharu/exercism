5886f60c1a067aef6c3bd5069074f474879bf59e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-09-28 16:27:40 +0530

    Implement unit tests to check edge cases

---
+++ python/same-tree/same_tree_test.py
@@ -0,0 +1,14 @@
+"""Testing script to test if two binary trees are identical"""
+
+
+import unittest
+
+from same_tree import Tree, TreeNode
+
+class SameTreeTests:
+    """Test Suite to identify if two binary trees are identical"""
+    pass
+
+
+if __name__=='__main__':
+    unittest.main()
