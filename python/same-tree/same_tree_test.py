10a52309c21d5e5a233d0ca4e5992b647d91e3ba
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-09-28 17:51:12 +0530

    Skeleton structure of unit tests with edge cases

--- python/same-tree/same_tree_test.py
+++ python/same-tree/same_tree_test.py
@@ -5,10 +5,33 @@ import unittest
 
 from same_tree import Tree, TreeNode
 
-class SameTreeTests:
+
+class SameTreeTests(unittest.TestCase):
     """Test Suite to identify if two binary trees are identical"""
-    pass
+
+    def test_two_null_tree(self):
+        p = TreeNode(val=None)
+        q = TreeNode(val=None)
+        self.assertTrue(Tree().is_same_tree(p, q))
+
+    def test_two_simple_and_identical_binary_trees(self):
+        self.fail()
+
+    def test_two_simple_and_different_binary_trees(self):
+        self.fail()
+
+    def test_two_medium_and_identical_binary_trees(self):
+        self.fail()
+
+    def test_two_medium_and_different_binary_trees(self):
+        self.fail()
+
+    def test_two_large_and_identical_binary_trees(self):
+        self.fail()
+
+    def test_two_large_and_slightly_different_binary_trees(self):
+        self.fail()
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     unittest.main()
