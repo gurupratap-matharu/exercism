57cbf8d7c7f8ac5b1aed9484402312a0d16c3c60
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-10-04 16:08:56 +0530

    Add test cases to verify edge and corner cases

---
+++ python/stairs/stairs_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from stairs import Stairs
+
+class StairsTests(unittest.TestCase):
+    """Test Suite to check distinct ways to take n stairs"""
+
+    pass
+
+
+if __name__=='__main__':
+    unittest.main()
