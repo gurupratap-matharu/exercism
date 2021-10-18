6a6c596b26f1e3c7cd74aea13d36ffcbfe560508
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-10-18 10:46:02 +0530

    Formatted using black

--- python/reverse-vowels/reverse_vowels.py
+++ python/reverse-vowels/reverse_vowels.py
@@ -6,21 +6,25 @@ class StringUtils:
 
     def reverse_vowels(self, text: str) -> str:
         """
-        Reverses only the vowels of a string.
+        Reverses only the vowels of a string using
+        the two pointer technique.
+
         Ex:
             'hello' -> 'holle'
             'leetcode' -> 'leotcede'
         """
+
         text = list(text)
 
         i, j = 0, len(text) - 1
-        is_vowel = lambda x: x.lower() in 'aeiou'
+        is_vowel = lambda x: x.lower() in "aeiou"
 
         while i < j:
+
             is_left_vowel = is_vowel(text[i])
             is_right_vowel = is_vowel(text[j])
 
-            if (is_left_vowel and is_right_vowel):
+            if is_left_vowel and is_right_vowel:
                 # perform the swap
                 text[i], text[j] = text[j], text[i]
                 i += 1
@@ -32,9 +36,9 @@ class StringUtils:
             if not is_right_vowel:
                 j -= 1
 
-        return ''.join(text)
+        return "".join(text)
 
 
-if __name__ == '__main__':
-    print(StringUtils().reverse_vowels('hello'))
-    print(StringUtils().reverse_vowels('leetcode'))
+if __name__ == "__main__":
+    print(StringUtils().reverse_vowels("hello"))
+    print(StringUtils().reverse_vowels("leetcode"))
