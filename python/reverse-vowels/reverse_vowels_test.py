9a81f5a37be6a991c27cfbfac3f1317a111748a2
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2021-10-18 10:46:15 +0530

    Added test cases to check corner cases

--- python/reverse-vowels/reverse_vowels_test.py
+++ python/reverse-vowels/reverse_vowels_test.py
@@ -4,8 +4,21 @@ import unittest
 
 from reverse_vowels import StringUtils
 
+
 class StringUtilsTests(unittest.TestCase):
     """Test suite to check edge cases while manipulating strings"""
 
     def test_reverse_vowels_on_blank_string(self):
-        self.assertEqual(StringUtils().reverse_vowels(text=''), '')
+        self.assertEqual(StringUtils().reverse_vowels(text=""), "")
+
+    def test_reverse_vowels_on_small_string(self):
+        self.assertEqual(StringUtils().reverse_vowels(text="hello"), "holle")
+
+    def test_reverse_vowels_on_string_with_repeated_vowels(self):
+        self.assertEqual(StringUtils().reverse_vowels(text="aaeeiioouu"), "uuooiieeaa")
+
+    def test_reverse_vowels_on_string_with_uppercase_letters(self):
+        self.assertEqual(StringUtils().reverse_vowels(text="HELLO"), "HOLLE")
+
+    def test_reverse_vowels_on_string_with_mixed_case_letters(self):
+        self.assertEqual(StringUtils().reverse_vowels(text="lEeTCOde"), "leOTCedE")
