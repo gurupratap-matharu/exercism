86b9e1345203f17a7ac1f6ddb49960efe8c4cd41
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2022-01-09 14:22:18 +0530

    linting the file

--- python/binary-find/binary-find.py
+++ python/binary-find/binary-find.py
@@ -8,18 +8,21 @@ class ArrayUtils:
         binary search. If target is not found then returns -1
         """
 
+        if len(nums) == 1 and nums[0] == target:
+            return 0
+
         def condition(value) -> bool:
             return nums[value] <= target
 
-        left, right = 0, len(nums)
+        left, right = 0, len(nums) - 1
 
         while left < right:
-            mid = left + (right - left) // 2
+            mid = (left + right) // 2
 
             if condition(mid):
-                right = mid
-            else:
                 left = mid + 1
+            else:
+                right = mid
 
         return left
 
@@ -27,3 +30,7 @@ class ArrayUtils:
 if __name__ == '__main__':
     utils = ArrayUtils()
     assert utils.binary_search(nums=[1, 2, 3, 4, 5], target=4), 3
+    assert utils.binary_search(nums=[1, 2, 3, 4, 5], target=6), -1
+    assert utils.binary_search(nums=[-1, 0, 3, 5, 9, 12], target=9), 4
+    print(utils.binary_search(nums=[-2, 0, 3, 5, 9, 12], target=13))
+    utils.binary_search(nums=[2, 5], target=5), 1
